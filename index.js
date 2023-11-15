ba9201ff9e3fa0ee107a4eb0cc20a1b4882e1b09
Carbon1769 <92681803+Carbon16@users.noreply.github.com>
2023-11-15 11:19:17 +0000

    Add audit log

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const app = express();
 const port = 8080;
 
 var names = []
+var tokens = []
+var logs = []
 
 function checkPresence(name) {
     //check names for the presence of name
@@ -15,7 +17,11 @@ function checkPresence(name) {
     return false;
 }
 
-app.get('/delete/:name', (req, res) => {
+app.get('/log', (req, res) => {
+    res.send(logs)
+});
+
+app.get('/delete/:name/:usr', (req, res) => {
     //check if id is already in names
     if (checkPresence(req.params.name)) {
         //remove name from names
@@ -25,6 +31,11 @@ app.get('/delete/:name', (req, res) => {
             }
         }
         res.sendStatus(200);
+        //timestamp
+        var date = new Date();
+        var time = date.getTime();
+        var dateStr = date.toString()
+        logs.push(`${dateStr}: ${req.params.usr} removed ${req.parms.name}`)
     };
 });
 
